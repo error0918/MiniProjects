87beac334f64c07744240b69004725cea63581c3
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-01-19 06:00:29 +0900

    2023/01/19 06:00

--- WowPhoneNumber/app/src/main/java/com/taeyeon/wowphonenumber/theme/Theme.kt
+++ WowPhoneNumber/app/src/main/java/com/taeyeon/wowphonenumber/theme/Theme.kt
@@ -1,19 +1,12 @@
 package com.taeyeon.wowphonenumber.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 private val LightColorScheme = lightColorScheme(
@@ -85,22 +78,23 @@ private val DarkColorScheme = darkColorScheme(
 fun WowPhoneNumberTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = false,
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> if (darkTheme) dynamicDarkColorScheme(LocalContext.current) else dynamicLightColorScheme(LocalContext.current)
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = colorScheme.primaryContainer,
+            darkIcons = !darkTheme
+        )
+        systemUiController.setNavigationBarColor(
+            color = colorScheme.primaryContainer,
+            darkIcons = !darkTheme
+        )
     }
 
     MaterialTheme(
--- WowPhoneNumber/app/src/main/java/com/taeyeon/wowphonenumber/ui/MainScreen.kt
+++ WowPhoneNumber/app/src/main/java/com/taeyeon/wowphonenumber/ui/MainScreen.kt
@@ -4,13 +4,18 @@
 package com.taeyeon.wowphonenumber.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.taeyeon.wowphonenumber.R
 import com.taeyeon.wowphonenumber.model.MainViewModel
 
@@ -20,15 +25,119 @@ fun MainContent(
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = { TopBar() }
+        topBar = { TopBar() },
+        bottomBar = { BottomBar() }
     ) {
-
+        Text(text = "afds",
+        modifier = Modifier.padding(it))
     }
 }
 
 @Composable
 fun TopBar() {
-    LargeTopAppBar(
-        title = { Text(text = stringResource(id = R.string.app_name)) }
-    )
+    val borderColor = MaterialTheme.colorScheme.onPrimaryContainer
+    Surface(
+        modifier = Modifier
+            .drawBehind {
+                val width = 4.dp.toPx()
+                drawArc(
+                    color = borderColor,
+                    startAngle = 90f,
+                    sweepAngle = 90f,
+                    useCenter = false,
+                    topLeft = Offset(0f, size.height - 48.dp.toPx()),
+                    size = Size(48.dp.toPx(), 48.dp.toPx()),
+                    style = Stroke(
+                        width = width,
+                        cap = StrokeCap.Square
+                    )
+                )
+                drawLine(
+                    color = borderColor,
+                    start = Offset(24.dp.toPx(), size.height),
+                    end = Offset(size.width - 24.dp.toPx(), size.height),
+                    strokeWidth = width,
+                    cap = StrokeCap.Square
+                )
+                drawArc(
+                    color = borderColor,
+                    startAngle = 0f,
+                    sweepAngle = 90f,
+                    useCenter = false,
+                    topLeft = Offset(size.width - 48.dp.toPx(), size.height - 48.dp.toPx()),
+                    size = Size(48.dp.toPx(), 48.dp.toPx()),
+                    style = Stroke(
+                        width = width,
+                        cap = StrokeCap.Square
+                    )
+                )
+            }
+            .padding(bottom = 2.dp),
+        shape = RoundedCornerShape(
+            topStart = 0.dp,
+            topEnd = 0.dp,
+            bottomStart = 24.dp,
+            bottomEnd = 24.dp
+        )
+    ) {
+        LargeTopAppBar(
+            title = { Text(text = stringResource(id = R.string.app_name)) },
+            colors = TopAppBarDefaults.largeTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                scrolledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        )
+    }
+}
+
+@Composable
+fun BottomBar() {
+    val borderColor = MaterialTheme.colorScheme.onPrimaryContainer
+    Surface(
+        modifier = Modifier
+            .drawBehind {
+                val width = 4.dp.toPx()
+                drawArc(
+                    color = borderColor,
+                    startAngle = 180f,
+                    sweepAngle = 90f,
+                    useCenter = false,
+                    topLeft = Offset(0f, 0f),
+                    size = Size(48.dp.toPx(), 48.dp.toPx()),
+                    style = Stroke(width = width)
+                )
+                drawLine(
+                    color = borderColor,
+                    start = Offset(24.dp.toPx(), 0f),
+                    end = Offset(size.width - 24.dp.toPx(), 0f),
+                    strokeWidth = width
+                )
+                drawArc(
+                    color = borderColor,
+                    startAngle = 270f,
+                    sweepAngle = 90f,
+                    useCenter = false,
+                    topLeft = Offset(size.width - 48.dp.toPx(), 0f),
+                    size = Size(48.dp.toPx(), 48.dp.toPx()),
+                    style = Stroke(width = width)
+                )
+            }
+            .padding(top = 2.dp),
+        shape = RoundedCornerShape(
+            topStart = 24.dp,
+            topEnd = 24.dp,
+            bottomStart = 0.dp,
+            bottomEnd = 0.dp
+        )
+    ) {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ) {
+            
+        }
+    }
 }
