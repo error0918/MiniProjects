f02818f08cc28432eb6551d8cf2c6a111c025ef3
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-01-27 18:51:45 +0900

    2023/01/27 18:51

--- WowPhoneNumber/app/src/main/java/com/taeyeon/wowphonenumber/ui/InfoDialog.kt
+++ WowPhoneNumber/app/src/main/java/com/taeyeon/wowphonenumber/ui/InfoDialog.kt
@@ -1,18 +1,36 @@
 package com.taeyeon.wowphonenumber.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.taeyeon.wowphonenumber.model.MainViewModel
 
+@Preview
 @Composable
 fun InfoDialog(
     mainViewModel: MainViewModel = MainViewModel(LocalContext.current).apply {
@@ -37,7 +55,61 @@ fun InfoDialog(
                 modifier = Modifier
                     .padding(28.dp)
             ) {
-                // TODO
+                BoxWithConstraints(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "앱 정보",
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier
+                            .align(Alignment.CenterStart)
+                            .padding(end = minHeight)
+                    )
+                    Image(
+                        painter = TODO(),
+                        contentDescription = null, // TODO
+                        modifier = Modifier
+                            .size(minHeight)
+                            .align(Alignment.CenterEnd)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+
+                // TODO: Body
+
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 4.dp)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Row(
+                    modifier = Modifier.align(Alignment.End),
+                    horizontalArrangement = Arrangement.spacedBy(space = 8.dp)
+                ) {
+                    CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.primary) {
+                        TextButton(
+                            onClick = { /* TODO */ }
+                        ) {
+                            Text(text = "오픈소스 라이선스")
+                        }
+                        TextButton(
+                            onClick = { mainViewModel.isInfoDialog = false }
+                        ) {
+                            Text(text = "닫기")
+                        }
+                    }
+                }
             }
         }
     }
