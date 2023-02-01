76e81bf0b8d52cf5f09f8238f2cb1ae1bd3f00f4
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-02 07:16:48 +0900

    2023/02/02 07:16

--- IconViewer/app/src/main/java/com/taeyeon/iconviewer/MainActivity.kt
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/MainActivity.kt
@@ -6,32 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.taeyeon.iconviewer.theme.IconViewerTheme
+import com.taeyeon.iconviewer.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             IconViewerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                    color = MaterialTheme.colorScheme.background,
+                    content = { MainScreen() }
+                )
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
---
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
@@ -0,0 +1,6 @@
+package com.taeyeon.iconviewer.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {}
