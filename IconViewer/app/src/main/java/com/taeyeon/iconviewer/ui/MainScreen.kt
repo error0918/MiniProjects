44bf06470aebc578b9a64fe856af3f9d31040f92
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-02 07:22:17 +0900

    2023/02/02 07:22

--- IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
@@ -1,6 +1,28 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.taeyeon.iconviewer.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun MainScreen() {}
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopBar() },
+        bottomBar = { BottomBar() },
+        //floatingActionButton = {},
+        //floatingActionButtonPosition =
+    ) { paddingValues ->  
+        
+    }
+}
+
+@Composable
+fun TopBar() {}
+
+@Composable
+fun BottomBar() {}
