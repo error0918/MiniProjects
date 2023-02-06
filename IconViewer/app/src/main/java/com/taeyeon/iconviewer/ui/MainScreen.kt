d03b0f9839578889641277a225c318a58c4e4e56
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-07 06:23:46 +0900

    2023/02/07 06:23

--- IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
@@ -1,11 +1,20 @@
 @file:OptIn(
-    ExperimentalMaterial3Api::class
+    ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class
 )
 
 package com.taeyeon.iconviewer.ui
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.with
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -24,6 +33,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
@@ -32,6 +42,7 @@ import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.KeyboardArrowUp
 import androidx.compose.material.icons.rounded.MoreVert
@@ -60,7 +71,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -268,36 +282,128 @@ fun TopBar(
     viewModel: IconViewerViewModel = IconViewerViewModel(state = rememberIconViewerState())
 ) {
     val scope = rememberCoroutineScope()
+    val density = LocalDensity.current
 
-    MediumTopAppBar(
-        title = { Text(text = stringResource(id = R.string.app_name)) },
-        actions = {
-            IconButton(
-                onClick = {
-                    scope.launch {
-                        viewModel.state.topAppBarScrollBehavior.state.collapse()
-                        viewModel.isSearching = !viewModel.isSearching
-                    }
+    AnimatedContent(
+        targetState = viewModel.isSearching,
+        transitionSpec = {
+            density.run {
+                if (viewModel.isSearching) {
+                    slideInHorizontally(
+                        animationSpec = tween(durationMillis = 250),
+                        initialOffsetX = { it - Icons.Rounded.Search.defaultWidth.toPx().toInt() * 2 }
+                    ) + fadeIn(
+                        animationSpec = tween(durationMillis = 250)
+                    ) with
+                            slideOutHorizontally(
+                                animationSpec = tween(durationMillis = 250),
+                                targetOffsetX = { - it + Icons.Rounded.Search.defaultWidth.toPx().toInt() * 2  }
+                            ) + fadeOut(
+                        animationSpec = tween(durationMillis = 250)
+                            )
+                } else {
+                    slideInHorizontally(
+                        animationSpec = tween(durationMillis = 250),
+                        initialOffsetX = { - it + Icons.Rounded.Search.defaultWidth.toPx().toInt() * 2 }
+                    ) + fadeIn(
+                        animationSpec = tween(durationMillis = 250)
+                    ) with
+                            slideOutHorizontally(
+                                animationSpec = tween(durationMillis = 250),
+                                targetOffsetX = { it - Icons.Rounded.Search.defaultWidth.toPx().toInt() * 2  }
+                            ) + fadeOut(
+                        animationSpec = tween(durationMillis = 250)
+                            )
                 }
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.Search,
-                    contentDescription = stringResource(id = R.string.main_top_bar_search)
-                )
-            }
-            IconButton(
-                onClick = {
-                    viewModel.isDropDownMenuShowing = !viewModel.isDropDownMenuShowing
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.MoreVert,
-                    contentDescription = stringResource(id = R.string.main_top_bar_more)
-                )
             }
         },
-        scrollBehavior = viewModel.state.topAppBarScrollBehavior
-    )
+        modifier = Modifier.background(MaterialTheme.colorScheme.surfaceColorAtElevation(elevation = 3.dp))
+    ) {
+        if (it) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .background(MaterialTheme.colorScheme.surfaceColorAtElevation(elevation = 3.dp))
+            ) {
+                // TODO
+                IconButton(
+                    onClick = {
+                        viewModel.isSearching = !viewModel.isSearching
+                    },
+                    modifier = Modifier
+                        .padding(
+                            start = 16.dp,
+                            end = 24.dp
+                        )
+                        .align(Alignment.CenterStart)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Search,
+                        contentDescription = stringResource(id = R.string.main_top_bar_search)
+                    )
+                }
+                BasicTextField(
+                    value = "TEST DATA",
+                    onValueChange = {},
+                    textStyle = MaterialTheme.typography.titleLarge.copy(
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    singleLine = true,
+                    cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurfaceVariant),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 48.dp + 24.dp)
+                        .align(Alignment.Center)
+                )
+                IconButton(
+                    onClick = { /* TODO */ },
+                    modifier = Modifier
+                        .padding(
+                            start = 24.dp,
+                            end = 16.dp
+                        )
+                        .align(Alignment.CenterEnd)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.ChevronRight,
+                        contentDescription = stringResource(id = R.string.main_top_bar_search_go)
+                    )
+                }
+            }
+        } else {
+            MediumTopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            scope.launch {
+                                viewModel.isDropDownMenuShowing = false
+                                viewModel.state.topAppBarScrollBehavior.state.collapse()
+                                viewModel.isSearching = !viewModel.isSearching
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Search,
+                            contentDescription = stringResource(id = R.string.main_top_bar_search)
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            viewModel.isDropDownMenuShowing = !viewModel.isDropDownMenuShowing
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.MoreVert,
+                            contentDescription = stringResource(id = R.string.main_top_bar_more)
+                        )
+                    }
+                },
+                scrollBehavior = viewModel.state.topAppBarScrollBehavior
+            )
+        }
+    }
 }
 
 @Composable
