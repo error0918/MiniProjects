f1e3eb8c5f002539e875ad63c053c0ef43445849
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-09 12:51:45 +0900

    2023/02/09 12:51

--- FancyScrollController/app/src/main/java/com/taeyeon/fancyscrollcontroller/ui/MainScreen.kt
+++ FancyScrollController/app/src/main/java/com/taeyeon/fancyscrollcontroller/ui/MainScreen.kt
@@ -1,5 +1,10 @@
 package com.taeyeon.fancyscrollcontroller.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
@@ -21,6 +26,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,14 +35,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.taeyeon.fancyscrollcontroller.R
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,9 +54,22 @@ fun MainScreen() {
     val topAppBarScrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     val scrollState = rememberScrollState()
     val scope = rememberCoroutineScope()
-
+    val hapticFeedback = LocalHapticFeedback.current
     var scrollAreaHeight by remember { mutableStateOf(0) }
     var scrollControllerAreaHeight by remember { mutableStateOf(0) }
+    var scrollControllerShowTime by remember { mutableStateOf(0L) }
+    var isScrollControllerShowing by remember { mutableStateOf(false) }
+
+    LaunchedEffect(scrollControllerShowTime) {
+        if (scrollControllerShowTime != 0L) {
+            isScrollControllerShowing = true
+            delay(2000)
+            if (scrollControllerShowTime + 1500 < System.currentTimeMillis()) isScrollControllerShowing = false
+        }
+    }
+    LaunchedEffect(scrollState.isScrollInProgress) {
+        if (scrollState.isScrollInProgress) scrollControllerShowTime = System.currentTimeMillis()
+    }
 
     Scaffold(
         modifier = Modifier
@@ -75,61 +97,76 @@ fun MainScreen() {
                     textAlign = TextAlign.Center
                 )
             }
-            Box(
+            AnimatedVisibility(
+                visible = isScrollControllerShowing,
                 modifier = Modifier
                     .width(24.dp)
                     .fillMaxHeight()
                     .align(Alignment.CenterEnd)
-                    .padding(4.dp)
-                    .onSizeChanged { scrollAreaHeight = it.height }
+                    .padding(4.dp),
+                enter = fadeIn() + slideInHorizontally { it },
+                exit = fadeOut() + slideOutHorizontally { it }
             ) {
-                Surface(
-                    color = MaterialTheme.colorScheme.primary,
-                    shape = CircleShape,
-                    shadowElevation = 5.dp,
+                Box(
                     modifier = Modifier
-                        .width(16.dp)
-                        .height(42.dp)
-                        .offset(y = LocalDensity.current.run { ((scrollAreaHeight - scrollControllerAreaHeight) * scrollState.value / scrollState.maxValue).toDp() })
-                        .onSizeChanged { scrollControllerAreaHeight = it.height }
-                        .pointerInput(Unit) {
-                            detectDragGestures(
-                                onDrag = { change, dragAmount ->
-                                    change.consume()
-                                    scope.launch {
-                                        scrollState.scrollTo(scrollState.value + (scrollState.maxValue * dragAmount.y / scrollAreaHeight).toInt())
+                        .fillMaxSize()
+                        .onSizeChanged { scrollAreaHeight = it.height }
+                ) {
+                    Surface(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = CircleShape,
+                        shadowElevation = 5.dp,
+                        modifier = Modifier
+                            .width(16.dp)
+                            .height(42.dp)
+                            .offset(y = LocalDensity.current.run { ((scrollAreaHeight - scrollControllerAreaHeight) * scrollState.value / scrollState.maxValue).toDp() })
+                            .onSizeChanged { scrollControllerAreaHeight = it.height }
+                            .pointerInput(Unit) {
+                                detectDragGestures(
+                                    onDragStart = {
+                                        hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                                    },
+                                    onDrag = { change, dragAmount ->
+                                        change.consume()
+                                        scope.launch {
+                                            scrollState.scrollTo(scrollState.value + (scrollState.maxValue * dragAmount.y / scrollAreaHeight).toInt())
+                                            scrollControllerShowTime = System.currentTimeMillis()
+                                        }
                                     }
-                                }
+                                )
+                            }
+                    ) {
+                        val contentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.8f)
+                        Canvas(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(
+                                    vertical = 6.dp,
+                                    horizontal = 4.dp
+                                )
+                        ) {
+                            drawPath(
+                                path = Path().apply {
+                                    moveTo(this@Canvas.size.width / 2f, 0f)
+                                    lineTo(0f, 4.dp.toPx())
+                                    lineTo(this@Canvas.size.width, 4.dp.toPx())
+                                    close()
+                                },
+                                color = contentColor
+                            )
+                            drawPath(
+                                path = Path().apply {
+                                    moveTo(this@Canvas.size.width / 2f, this@Canvas.size.height)
+                                    lineTo(0f, this@Canvas.size.height - 4.dp.toPx())
+                                    lineTo(
+                                        this@Canvas.size.width,
+                                        this@Canvas.size.height - 4.dp.toPx()
+                                    )
+                                    close()
+                                },
+                                color = contentColor
                             )
                         }
-                ) {
-                    val contentColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.8f)
-                    Canvas(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(
-                                vertical = 6.dp,
-                                horizontal = 4.dp
-                            )
-                    ) {
-                        drawPath(
-                            path = Path().apply {
-                                moveTo(this@Canvas.size.width / 2f, 0f)
-                                lineTo(0f, 4.dp.toPx())
-                                lineTo(this@Canvas.size.width, 4.dp.toPx())
-                                close()
-                            },
-                            color = contentColor
-                        )
-                        drawPath(
-                            path = Path().apply {
-                                moveTo(this@Canvas.size.width / 2f, this@Canvas.size.height)
-                                lineTo(0f, this@Canvas.size.height - 4.dp.toPx())
-                                lineTo(this@Canvas.size.width, this@Canvas.size.height - 4.dp.toPx())
-                                close()
-                            },
-                            color = contentColor
-                        )
                     }
                 }
             }
