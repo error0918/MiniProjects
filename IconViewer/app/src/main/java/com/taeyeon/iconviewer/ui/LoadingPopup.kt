1b0161a65c7cd7ff7dee1f002cae94e01f50de0c
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-12 18:36:02 +0900

    2023/02/12 18:35

---
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/LoadingPopup.kt
@@ -0,0 +1,10 @@
+package com.taeyeon.iconviewer.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Popup
+
+@Composable
+fun LoadingPopup() {
+    Popup() {
+    }
+}
