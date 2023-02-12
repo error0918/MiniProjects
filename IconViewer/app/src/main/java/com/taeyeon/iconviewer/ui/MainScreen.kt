a254557b47b13b2ddbb0490695be50e56d6cf393
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-12 11:14:20 +0900

    Update MainScreen.kt

--- IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/MainScreen.kt
@@ -10,12 +10,14 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -109,6 +111,15 @@ fun MainScreen(
                 itemSpace = (maxWidth - itemWidth * itemColumns) / (itemColumns + 1)
             }
 
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                //state =,
+                //verticalArrangement =,
+                //flingBehavior =,
+            ) {}
+
+            // Old
+            /*
             if (itemColumns > 0) {
                 Column(
                     modifier = Modifier.verticalScroll(state = viewModel.state.bodyScrollState)
@@ -209,6 +220,7 @@ fun MainScreen(
 
                 }
             }
+             */
 
             ScrollController(viewModel = viewModel)
         }
