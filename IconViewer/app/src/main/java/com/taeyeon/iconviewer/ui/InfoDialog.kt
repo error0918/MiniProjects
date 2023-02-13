87b73c57e4343876e1a10d24dd8be37289724fa7
정태연 Jtaeyeon <jtaeyeon05@gmail.com>
2023-02-13 11:50:57 +0900

    2023/02/13 11:50

--- IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/InfoDialog.kt
+++ IconViewer/app/src/main/java/com/taeyeon/iconviewer/ui/InfoDialog.kt
@@ -1,9 +1,13 @@
 package com.taeyeon.iconviewer.ui
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.window.DialogProperties
 import com.taeyeon.iconviewer.IconViewerViewModel
+import com.taeyeon.iconviewer.R
 import com.taeyeon.iconviewer.rememberIconViewerState
 
 @Composable
@@ -16,7 +20,13 @@ fun InfoDialog(
             icon = {},
             title = {},
             text = {},
-            dismissButton = {},
+            dismissButton = {
+                TextButton(
+                    onClick = { viewModel.isInfoShowing = false }
+                ) {
+                    Text(text = stringResource(id = R.string.info_dialog_dismiss))
+                }
+            },
             confirmButton = { /*TODO*/ },
             properties = DialogProperties(
                 dismissOnBackPress = true,
