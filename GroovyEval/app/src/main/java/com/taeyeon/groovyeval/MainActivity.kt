ec8c2af9ed4aab929344880f7f46ac46b40ed3f5
jtaeyeon05 <jtaeyeon05@gmail.com>
2023-03-22 15:04:00 +0900

    Groovy Eval - 2023/03/22 15:03

---
+++ GroovyEval/app/src/main/java/com/taeyeon/groovyeval/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.taeyeon.groovyeval
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.taeyeon.groovyeval.ui.theme.GroovyEvalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GroovyEvalTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
