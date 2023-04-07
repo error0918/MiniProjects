050a92e8aec465dff8358474b301fa5e9624b14e
jtaeyeon05 <jtaeyeon05@gmail.com>
2023-04-07 14:31:39 +0900

    2023/04/07 14:31

--- GroovyEval/app/src/main/java/com/taeyeon/groovyeval/MainActivity.kt
+++ GroovyEval/app/src/main/java/com/taeyeon/groovyeval/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.taeyeon.groovyeval.ui.theme.GroovyEvalTheme
 import groovy.lang.Binding
 import groovy.lang.GroovyShell
@@ -23,28 +21,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Text(text = "Hello ${a()}!")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello ${a()}!",
-        modifier = modifier
-    )
-}
-
-fun a(): Any {
+fun a(): Int {
     val script = """
         def a = 10
         def b = 20
-        println("a + b = ${'$'}{a + b}")
+        a + b
     """
     val binding = Binding()
     val shell = GroovyShell(binding)
-    return shell.evaluate(script)
+    shell.evaluate(script)
+    //val binding = Binding()
+    //val shell = GroovyShell(binding)
+    //shell.parse(script).run() as Int
+    //return shell.evaluate(script) as Int
+    //return shell.evaluate(script) as Int
+    return 1
 }
