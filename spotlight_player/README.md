623a4226e08329c770fe78737e48fbbd48781d8c
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-07-15 02:23:31 +0900

    Init SpotlightPlayer

---
+++ spotlight_player/README.md
@@ -0,0 +1,7 @@
+# SpotlightPlayer
+
+<div align=right>
+        jtaeyeon05 (2025/07)
+</div>
+ 
+> Dart, Flutter
---
+++ spotlight_player/android/app/src/main/kotlin/com/jtaeyeon05/spotlight_player/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jtaeyeon05.spotlight_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
